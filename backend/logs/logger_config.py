d92be5f2f75cc8f90268c86d033f69bc6f0b18d9
Rylan Hiltz <rylanhiltz2@gmail.com>
2026-01-21 17:13:39 -0500

    refactor: update logging level to WARNING for improved log management

--- backend/logs/logger_config.py
+++ backend/logs/logger_config.py
@@ -20,7 +20,7 @@ def init_logger():
                 filename=file_path, maxBytes=5 * 1024 * 1024, backupCount=3
             )
         ],
-        level=logging.INFO,
+        level=logging.WARNING,
         format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
